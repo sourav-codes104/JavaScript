665be07e5aa82d12ff7f8b974dd40a7f162ba71e
sourav-codes104 <souravsingh.error104@gmail.com>
2025-12-22 17:39:37 +0530

    Day-12

---
+++ Day_12/script.js
@@ -0,0 +1,36 @@
+// let ans = document.getElementsByTagName("p");
+// console.dir(ans);
+
+// let ans = document.getElementsByClassName("heading");
+
+// let ans = document.getElementById("list");
+// console.dir(ans);
+
+// let ans = document.querySelector(".heading")
+
+// let ans= document.querySelector("title");
+// // console.dir(ans);
+
+// let parent = ans.parentElement
+// console.dir(parent);
+
+// let ans = document.querySelector("#list");
+
+
+
+// let first_child = ans.firstElementChild;
+// let last_child = ans.lastElementChild;
+// console.dir(first_child);
+// console.dir(last_child);
+
+// let tag = document.querySelector("#list");
+// let ans = tag.tagName
+
+// let inner_text = document.querySelector("#list");
+// let ans = inner_text.innerText;
+
+// let inner_HTML = document.querySelector("#list");
+// let ans = inner_HTML.innerHTML;
+
+// let text_content = document.querySelector("#list");
+// let ans = text_content.textContent;

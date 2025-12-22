50cd3c1012125c27c86a1688b4a626e891d7a748
sourav-codes104 <souravsingh.error104@gmail.com>
2025-12-22 21:01:20 +0530

    Day-13

---
+++ Day_13/script.js
@@ -0,0 +1,23 @@
+// // let ans = document.querySelector("div");
+
+// // let attr = ans.getAttribute("rollno");
+
+// // ans.setAttribute("rollno","71");
+
+// // let new_attr = ans.getAttribute("rollno");
+
+// // ans.style.backgroundColor="red";
+
+// let btn = document.createElement("button");
+// // document.querySelector("div").append(btn);
+// // document.querySelector(".box").prepend(btn);
+// document.querySelector(".box").before(btn);
+// // document.querySelector("div").append(btn);
+// btn.innerText = "click me";
+
+// let head = document.createElement("h1");
+// head.innerHTML="<U><I>Wow what a beautiful day</I></U>";
+
+// document.querySelector(".main_box").before(head);
+
+// document.querySelector("h1").remove();

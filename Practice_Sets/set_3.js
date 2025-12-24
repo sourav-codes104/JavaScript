3f9b143193306c17de1a7dcf4940b9bfdb1bf580
sourav-codes104 <souravsingh.error104@gmail.com>
2025-12-24 08:45:52 +0530

    set-03

--- Practice_Sets/set_3.js
+++ Practice_Sets/set_3.js
@@ -8,3 +8,28 @@
 // Check if array is palindrome
 // Split array into chunks of size k
 // Remove element at a given index without using splice()
+
+// 1 Find the missing number in an array of 1…n
+// let arr = [1,2,3,4,6,7,8,10,12,16,18,19,23,24,26,29,30,33,34,35,37];
+// let max = 38;
+// let set = new Set(arr);
+// let missing = [];
+// for(let i=1;i<=max;i++){
+//   if(!set.has(i)){
+//     missing.push(i);
+//   }
+// }
+// console.log(missing);
+
+// 2 Find all pairs with sum = target
+// let arr = [[4,5],[6,7],[3,6],[4,4],[9,0]];
+// let target = 9, sum = 0;
+// let pair=[];
+// // console.log(arr);
+// for(let i=0; i<arr.length; i++){
+//   sum = arr[i][0]+arr[i][1];
+//   if(sum===target){
+//     pair.push(arr[i]);
+//   }
+// }
+// console.log(pair);
